7409e5350e09dfaa5d2f6cef5d700cef37c10d1e
haveyouwantto <haveyouwantto@gmail.com>
2021-11-08 18:41:31 +0800

    simplify code

--- instruments/manifest.js
+++ instruments/manifest.js
@@ -1,10 +1,16 @@
-let inst = [
-    {
+let insts = {
+    "windsong_lyre": {
         "id": "windsong_lyre",
-        "name": "风物之诗琴"
+        "name": "风物之诗琴",
+        "color": "rgb(88, 153, 144)",
+        "bg1": "rgb(144, 249, 227)",
+        "bg2": "rgba(144, 249, 227, 0.8)"
     },
-    {
+    "floral_zither": {
         "id": "floral_zither",
-        "name": "镜花之琴"
+        "name": "镜花之琴",
+        "color": "rgb(173, 159, 146)",
+        "bg1": "rgb(221, 203, 161)",
+        "bg2": "rgba(221, 203, 161, 0.8)"
     }
-];
+};
